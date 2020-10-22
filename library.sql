62381385bcbc67fe7d3e727c282d4ab15189105f
grantk00 <73309163+grantk00@users.noreply.github.com>
2020-10-22 16:53:44 -0400

    Add files via upload
    group task 1, library database for book and loans

---
+++ library.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Book (
+    ISBN int,
+    Title string,
+    Author string,
+    Call_Number string,
+    Pages int
+    );
+
+CREATE TABLE Loans (
+    ISBN int,
+    Patron string
+    );
+
