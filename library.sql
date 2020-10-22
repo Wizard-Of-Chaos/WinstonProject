e6418cfb6e000423fcea6ae38c549a03a1ea99dc
Michael Eddins <michaeleddins4@ritchie.cs.longwood.edu>
2020-10-22 19:00:08 -0400

    Put up the library.sql this time too

--- library.sql
+++ library.sql
@@ -11,3 +11,14 @@ CREATE TABLE Loans (
     Patron varchar(255)
     );
 
+INSERT INTO Book (ISBN, Title, Author, Call_Number, Pages)
+  VALUES (1234567, "Cheese", "Michael Eddins", "13131313", 90);
+
+INSERT INTO Book 
+  VALUES (8005882300, "Empire", "Grant Kelly", "8675309AB", 300);
+
+INSERT INTO Loans
+  VALUES (90604010, "Big Chungus");
+
+INSERT INTO Loans
+  VALUES (345345894689, "Epic Style");
