855b654247a6dcbd75a48f670b5405a25c99e6d2
grantk00 <73309163+grantk00@users.noreply.github.com>
2020-10-31 22:05:09 -0400

    SQL with binding and publisher

--- library.sql
+++ library.sql
@@ -3,7 +3,9 @@ CREATE TABLE Book (
     Title varchar(255),
     Author varchar(255),
     Call_Number varchar(255),
-    Pages int
+    Pages int,
+    Binding varchar(255),
+    Publisher varchar(255)
     );
 
 CREATE TABLE Loans (
@@ -11,14 +13,14 @@ CREATE TABLE Loans (
     Patron varchar(255)
     );
 
-INSERT INTO Book (ISBN, Title, Author, Call_Number, Pages)
-  VALUES (1234567, 'Cheese', 'Michael Eddins', '13131313', 90);
+INSERT INTO Book (ISBN, Title, Author, Call_Number, Pages, Binding, Publisher)
+  VALUES (1234567, 'Cheese', 'Michael Eddins', '13131313', 90, 'Paper Back', 'Penguin');
 
 INSERT INTO Book 
-  VALUES (8005882300, 'Empire', 'Grant Kelly', '8675309AB', 300);
+  VALUES (8005882300, 'Empire', 'Grant Kelly', '8675309AB', 300, 'Hard Back', 'Publisher2');
 
 INSERT INTO Book
-  VALUES (9090909090, 'Some Cool Name', 'Michael Eddins', '182749329248', 1000);
+  VALUES (9090909090, 'Some Cool Name', 'Michael Eddins', '182749329248', 1000, 'Periodical', 'Spielburg');
 
 INSERT INTO Loans
   VALUES (90604010, 'Big Chungus');
